3c72efb6b5be4e7137077068b4db4b532f4365e3
菜鸟e站 <15797675808@163.com>
2018-11-17 17:39:14 +0800

    Update config.js

--- wechat/config.js
+++ wechat/config.js
@@ -1,11 +1,11 @@
 module.exports =
  {  
     wechat: {   
-                appId: 'wxd2ee66f02097a03a',     
-                appserect: 'b8a0ccfc0b1e71220a176c50b9bd2fcb',
-                key: '13870656966huacai201813576106500',
-                mch_id: '1508869821',
-                IP: '139.199.165.202',
+                appId: 'wxd2er9g466020703a',     
+                appserect: 'b8agcjcf0gb1j71h22617j650b9d2fb',
+                key: '1387065694567fghjiuai20181356500',
+                mch_id: '1508678789',
+                IP: '100.189.125.108',
                 baseUrl: 'https://9b308d52.ngrok.io/huacai'
         }
  };
